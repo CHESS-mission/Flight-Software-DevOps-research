aef3505b8cc33153a0171cd20836f87b907dd762
maximewel <maxime.welcklen@he-arc.ch>
2020-11-24 14:55:40 +0100

    add working unit test script

---
+++ scripts/unit-test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#activate the python environnement
+. ./fprime-venv/bin/activate
+
+#check if an app name is specified
+if [[ -z "$1" ]]
+then
+	echo "Please specify your app name, exiting..."
+	exit 1
+fi
+
+#exit on error
+set -e
+
+#build the unit test
+cd $1
+for dir in ./*
+do
+	if [[ -d "$dir/test" ]]
+	then
+		echo "Detected component with unit test !"
+		echo "Building unit test : $dir"
+		cd $dir
+		fprime-util build --ut
+		echo "Testing $dir"
+		fprime-util check
+		cd ..
+	fi
+done
